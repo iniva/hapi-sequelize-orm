999762f85e2fba0d2c2733c3a6edf2aad543bf84
Tony <tonyblast@gmail.com>
2018-12-25 07:59:11 -0300

    refactor: Refactored 'index' to be the library exporter, not only for plugin

--- src/index.js
+++ src/index.js
@@ -1,41 +1,15 @@
-/* eslint-disable no-console */
-import { name, version } from '../package';
 import Orm from './orm';
+import plugin from './plugin';
+import {
+    ASSOCIATION_TYPES,
+    DATA_TYPES,
+    QUERY_TYPES
+} from './types';
 
-export default {
-    name,
-    version,
-    register: async (server, { databases }) => {
-        const getDb = dbName => {
-            if (!dbName) {
-                throw new Error(`${name}: You must provide a database name`);
-            }
-
-            const instancePlugin = `${name}:${dbName}`;
-
-            if (!server.hasOwnProperty(instancePlugin)) {
-                throw new Error(`${name}: There's no [${dbName}] database`);
-            }
-
-            return server[instancePlugin];
-        };
-
-        for (const dbName in databases) {
-            try {
-                const instance = new Orm(databases[dbName]);
-
-                await instance.testConnection();
-                await instance.init();
-
-                server.decorate('server', `${name}:${dbName}`, instance);
-                console.log(`${name}: [${dbName}] database initialized`);
-            }
-            catch (error) {
-                console.log(`${name}: Could not initialize plugin for [${dbName}] database`);
-                throw error;
-            }
-        }
-
-        server.decorate('server', name, { getDb });
-    }
+export {
+    Orm,
+    plugin,
+    ASSOCIATION_TYPES,
+    DATA_TYPES,
+    QUERY_TYPES
 }

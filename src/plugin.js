160cfb24c832297a00e98acf15c2cb42a431caac
Tony <tonyblast@gmail.com>
2018-12-25 07:58:07 -0300

    feat: Migrated all plugin functionality to 'plugin' file

---
+++ src/plugin.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-console */
+import { name, version } from '../package';
+import Orm from './orm';
+
+export default {
+    name,
+    version,
+    register: async (server, { databases }) => {
+        const getDb = dbName => {
+            if (!dbName) {
+                throw new Error(`${name}: You must provide a database name`);
+            }
+
+            const instancePlugin = `${name}:${dbName}`;
+
+            if (!server.hasOwnProperty(instancePlugin)) {
+                throw new Error(`${name}: There's no [${dbName}] database`);
+            }
+
+            return server[instancePlugin];
+        };
+
+        for (const dbName in databases) {
+            try {
+                const instance = new Orm(databases[dbName]);
+
+                await instance.testConnection();
+                await instance.init();
+
+                server.decorate('server', `${name}:${dbName}`, instance);
+                console.log(`${name}: [${dbName}] database initialized`);
+            }
+            catch (error) {
+                console.log(`${name}: Could not initialize plugin for [${dbName}] database`);
+                throw error;
+            }
+        }
+
+        server.decorate('server', name, { getDb });
+    }
+}
